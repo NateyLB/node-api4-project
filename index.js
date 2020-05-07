971d0f7ba29efa556d4edd6339f3fbc882167092
Nathan <nateyl.b@gmail.com>
2020-05-07 10:08:09 -0700

    switched some lines around to test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
+require('dotenv').config()
+const port = process.env.PORT;
 const server = require('./server.js');
 
-const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`\n* Server Running on http://localhost:${port} *\n`);
